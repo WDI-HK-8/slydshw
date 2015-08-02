aff6d596a8b52277921c468e6895d42506773ad7
teelu <terrence.lun@gmail.com>
2015-08-02 22:54:10 +0800

    feat(master) - Add MONGODB and YAR plugins

--- index.js
+++ index.js
@@ -23,6 +23,25 @@ server.views({
 
 plugins = [
   { register: require('./routes/static-pages.js')},
+  { register: require('yar'),
+    options: {
+      cookieOptions:{
+        password: process.env.COOKIE_PASSWORD || 'HelloHowAreYou',
+        isSecure: false
+      }
+    }
+  },
+  {
+    register: require('hapi-mongodb'),
+    options: {
+      url: process.env.MONGOLAB_URI || "mongodb://127.0.0.1:27017/slidshw",
+      settings: {
+        db: {
+          native_parser:false
+        }
+      }
+    }
+  }
 ];
 
 //start server
